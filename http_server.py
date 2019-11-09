139a9f19b7f7d8d7fcf084c8b42e701d70cc2244
zhaoyanz405 <zhaoyanz405@gmail.com>
2019-11-09 11:01:52 +0800

    feat: 修改几处错误，并修改adapt指定handler处理请求
    - register_handler应接受字典参数，因为url含有特殊字符，无法作为关键字
    - listen加入默认值128

--- http_server.py
+++ http_server.py
@@ -5,14 +5,13 @@
 @contact: zhaoyanz405@gmail.com
 @time: 2019/11/6 14:12
 """
-import datetime
 import json
 import socket
 import traceback
 from json import JSONDecodeError
 
 from exception import HTTPMethodInvalidError
-from http import HTTPRequest, HTTPHeader, HTTPResponse
+from base import HTTPRequest, HTTPHeader, HTTPResponse
 
 
 def parse(msg):
@@ -52,12 +51,12 @@ class HTTPServer:
         self.conn = None
         self.handler_map = {}
 
-    def register_handler(self, **kwargs):
-        self.handler_map.update(kwargs)
+    def register_handler(self, maps: dict):
+        self.handler_map.update(maps)
 
     def run(self):
         self.socket.bind((self.host, self.port))
-        self.socket.listen()
+        self.socket.listen(128)
         print('server has been started. ')
         self.conn, addr = self.socket.accept()
         print('listening at %s' % str(addr))
@@ -106,29 +105,21 @@ class HTTPServer:
         :param request:
         :return:
         """
-
-        print('------%s--------' % request.method)
         parse_query_params(request)
         parse_body_params(request)
-        print('request.params: ', request.params)
-        if request.method == 'GET':
-            response = HTTPResponse(status_code=200, status_msg='OK')
-            response.content = "<h1>testst</h1>"
-            response.header = HTTPHeader({
-                'Date': datetime.datetime.now().strftime("%a, %d %b %Y %H:%M:%S GMT"),
-                'Server': 'mars',
-                'Content-Type': 'text/html',
-                'Content-Length': len(response.content)
-            })
-
+        cls = self.handler_map.get(request.url)
+        if not cls:
+            response = HTTPResponse(status_code=404, status_msg='Not Found')
             self.conn.send(str(response).encode('utf-8'))
+            return
 
-        if request.method == 'POST':
-            response = HTTPResponse(status_code=200, status_msg='OK')
-            self.conn.send(str(response.response_line).encode('utf-8'))
-            print(request.header)
-            data = self.conn.recv(int(request.header['Content-Length']))
-            print(data)
+        try:
+            eval('cls(request, self.conn).%s()' % request.method.lower())
+        except Exception as e:
+            print(e)
+            print(traceback.format_exc())
+            response = HTTPResponse(status_code=500, status_msg='Server Error')
+            self.conn.send(str(response).encode('utf-8'))
 
     def stop(self):
         self.socket.close()
@@ -162,7 +153,7 @@ def parse_body_params(request: HTTPRequest):
     """
     print('body', type(request.body), request.body)
     content_type = request.header.get('Content-Type')
-    if content_type.find("json") >= 0:
+    if content_type and content_type.find("json") >= 0:
         try:
             request.params.update(json.loads(request.body))
         except JSONDecodeError:
